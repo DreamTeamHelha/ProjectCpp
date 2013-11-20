5733f3fcef6a59cc72f3541c6ddc2cb10c575227
RaphDesaegher <raphael.desaegher@gmail.com>
2013-11-20 12:20:59 +0100

    Ajout du chargement de la tilemap à partir du fichier image
    Fonctionnel

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include "gamewindow.h"
 #include "scene.h"
 #include "objectfactory.h"
+#include <QCoreApplication>
+#include "tilemaploader.h"
+#include "tilemap.h"
+
 
 int main(int argc, char ** argv)
 {
@@ -11,6 +15,21 @@ int main(int argc, char ** argv)
 
     gameWindow.setScene(&scene);
     scene.setPlayerInput(&gameWindow.playerInput());
+
+    Tilemap *tilemap;
+    tilemap = TilemapLoader::load(QCoreApplication::applicationDirPath() + "/data/tracks/Raph_paradise.png");
+    std::cout<<"Taille de la map :"<<tilemap->width()<<" x "<<tilemap->height()<<std::endl;
+    for(int i = 0;i<tilemap->width();i++)
+    {
+        for(int j=0;j<tilemap->height();j++)
+        {
+            std::cout<<(int)tilemap->tile(i,j);
+        }
+        std::cout<<std::endl;
+    }
+
+
+
 
     gameWindow.show();
     return app.exec();
