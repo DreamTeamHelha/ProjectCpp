d7d6675832f2242af4482e21a088d8de972f213d
Marié Paulino <paulinodjm@hotmail.com>
2013-11-20 12:53:36 +0100

    Initialise les objetctFactories dans le main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <QApplication>
 #include "gamewindow.h"
 #include "scene.h"
-#include "objectfactory.h"
+#include "objectfactories.h"
 
 int main(int argc, char ** argv)
 {
@@ -9,9 +9,19 @@ int main(int argc, char ** argv)
     GameWindow gameWindow;
     Scene scene;
 
+    // -- test -- paramétrage de la scène
     gameWindow.setScene(&scene);
     scene.setPlayerInput(&gameWindow.playerInput());
 
+    // initialisation des factories
+    ObjectFactories::initialize();
+
+    // exécution du programme
     gameWindow.show();
-    return app.exec();
+    int returnCode = app.exec();
+
+    // destruction des factories
+    ObjectFactories::finalize();
+
+    return returnCode;
 }
