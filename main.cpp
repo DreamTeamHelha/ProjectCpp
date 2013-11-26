ac8b44f93e03aeab4fe3f8768afb0d3d3e8753b0
Marié Paulino <paulinodjm@hotmail.com>
2013-11-26 20:16:42 +0100

    Remet le main dans un état visuellement acceptable.
    Parce que c'était vraiment le bordel!

--- main.cpp
+++ main.cpp
@@ -1,30 +1,24 @@
 #include <QApplication>
-#include "gamewidget.h"
-#include "scene.h"
 #include "objectfactories.h"
-#include "tilemaploader.h"
-#include <QCoreApplication>
-#include <QMessageBox>
-#include"mainwindow.h"
+#include "mainwindow.h"
+
 int main(int argc, char ** argv)
 {
     QApplication app(argc, argv);
+
+    // initialisation des factories
     ObjectFactories::initialize();
+
+    // création de la fenêtre et exécution
     MainWindow w;
     w.show();
     w.setMaximumSize(800,600);
     w.setMinimumSize(800,600);
 
-    //QWindow::setFixedSize(800,600);
     int returnCode = app.exec();
+
     // destruction des factories
     ObjectFactories::finalize();
-
-
-
-
-
-
 
     return returnCode;
 }
