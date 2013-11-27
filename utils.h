301c04ffdd546d87158bb203e877a6bee38e8028
RaphDesaegher <raphael.desaegher@gmail.com>
2013-11-27 11:55:21 +0100

    Correction du format d'affichage du temps
    2 chiffres sont affichés pour chaque valeur

--- utils.h
+++ utils.h
@@ -20,12 +20,17 @@ namespace utils
     }
     inline QString showableTime(int ms)
     {
+        QChar character(0x30);
         ms/=10;
         int csec = ms%100;
         ms/=100;
         int sec = ms%60;
         ms/=60;
-        QString time = QString::number(ms)+":"+QString::number(sec)+":"+QString::number(csec)+"''";
+        QString time;
+        time= QString ("%1:%2:%3")
+               .arg(ms,2,10,character)
+               .arg(sec,2,10,character)
+               .arg(csec,2,10,character);
         return time;
 
     }
